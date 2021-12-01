0011144fe9c0c17e7e9f50bf065df88180e98ff5
Gitea <mssblackhole@gmail.com>
2021-12-01 20:47:28 +0330

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace FileCat
+{
+    partial class mainFrm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.resultLst = new System.Windows.Forms.ListBox();
+            this.scanBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // resultLst
+            // 
+            this.resultLst.FormattingEnabled = true;
+            this.resultLst.Location = new System.Drawing.Point(4, 43);
+            this.resultLst.Name = "resultLst";
+            this.resultLst.Size = new System.Drawing.Size(413, 225);
+            this.resultLst.TabIndex = 0;
+            // 
+            // scanBtn
+            // 
+            this.scanBtn.Location = new System.Drawing.Point(4, 12);
+            this.scanBtn.Name = "scanBtn";
+            this.scanBtn.Size = new System.Drawing.Size(148, 23);
+            this.scanBtn.TabIndex = 1;
+            this.scanBtn.Text = "Move Files";
+            this.scanBtn.UseVisualStyleBackColor = true;
+            this.scanBtn.Click += new System.EventHandler(this.scanBtn_Click);
+            // 
+            // mainFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(417, 261);
+            this.Controls.Add(this.scanBtn);
+            this.Controls.Add(this.resultLst);
+            this.Name = "mainFrm";
+            this.Text = "Desktop Files Categurizer";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox resultLst;
+        private System.Windows.Forms.Button scanBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileCat
+{
+    public partial class mainFrm : Form
+    {
+        public mainFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void scanBtn_Click(object sender, EventArgs e)
+        {
+
+
+            string dirPath = Environment.ExpandEnvironmentVariables("%userprofile%\\Desktop");
+            DirectoryInfo dinfo = new DirectoryInfo(dirPath);
+            FileInfo[] files = dinfo.GetFiles("*");
+            resultLst.Items.Clear();
+            List<String> list = new List<string>();
+
+
+            foreach (FileInfo file in files)
+            {
+
+                if (new Regex("^@").IsMatch(file.Name))
+                {
+                    continue;
+                }
+
+
+                if (file.Extension == ".lnk")
+                {
+                    continue;
+                }
+
+                if (file.Name=="desktop.ini") {
+                    continue;
+                }
+
+                if (file.FullName == System.Reflection.Assembly.GetEntryAssembly().Location)
+                {
+                    continue;
+                }
+
+
+                String tagrgetDir;
+
+                if (file.Extension == "")
+                {
+                    tagrgetDir = dirPath + "\\Folders\\Others";
+                }
+                else
+                {
+                    tagrgetDir = dirPath + "\\Folders\\" + file.Extension.Substring(1);
+                }
+
+                if (!Directory.Exists(tagrgetDir))
+                {
+                    Directory.CreateDirectory(tagrgetDir);
+                }
+
+                String targetFile = tagrgetDir + "\\" + file.Name;
+
+                if (File.Exists(targetFile))
+                {
+                    targetFile = tagrgetDir + "\\" +Path.GetFileNameWithoutExtension(targetFile) + "_" + new Random().Next(1000) + file.Extension;
+                    //MessageBox.Show(targetFile);
+                    //return;
+                }
+
+                try
+                {
+                    File.Move(file.FullName, targetFile);
+                }
+                catch (IOException ioe)
+                {
+                    MessageBox.Show($"`{file.Name}` {ioe.Message}");
+                }
+
+
+                //MessageBox.Show(file.FullName);
+
+                resultLst.Items.Add(file.Name);
+                list.Add(file.Name);
+            }
+        }
+    }
+}
